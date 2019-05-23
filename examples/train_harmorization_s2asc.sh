cb99bd2295bfb78a472ffcfe9a8348055d99bf90
Shadow Cun <vinthony@gmail.com>
2019-05-23 10:55:58 +0800

    Create train_harmorization_s2asc.sh

---
+++ examples/train_harmorization_s2asc.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=1 python main.py\
+  --epochs 60\
+  --schedule 60\
+  --lr 1e-3\
+  -c psnr/cocov4/1e3_bs8_256\
+  --arch rascv1\
+  --machine basic\
+  --input-size 256\
+  --limited-dataset 0\
+  --train-batch 8\
+  --test-batch 1\
+  --base-dir $HOME/Datasets/scoco/\
+  --data 2017
