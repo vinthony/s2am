0971be07e87adda02f72559a4cd4f71a265c973f
Shadow Cun <vinthony@gmail.com>
2019-05-23 11:11:22 +0800

    Update __init__.py

--- scripts/models/__init__.py
+++ scripts/models/__init__.py
@@ -1,9 +1,4 @@
-from .dhn import *
 from .backbone_unet import *
-from .backbone_vgg import *
-from .backbone_atrous import *
-from .backbone_can import *
 from .discriminator import *
-from .globalattentionnetwork import *
-from .partialunet import *
+
 
